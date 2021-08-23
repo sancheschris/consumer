531c876cf06c030a2961378590431fc84f6e5dc4
cnsd <chris.sanchees@hotmail.com>
2021-08-23 12:38:47 -0300

    Initial commit

---
+++ src/main/java/com/live/kafka/consumer/consumer/CarConsumer.java
@@ -0,0 +1,27 @@
+package com.live.kafka.consumer.consumer;
+
+import com.live.kafka.consumer.DTO.CarDTO;
+import lombok.extern.slf4j.Slf4j;
+import org.apache.kafka.clients.consumer.ConsumerRecord;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.kafka.annotation.KafkaListener;
+import org.springframework.stereotype.Component;
+
+@Component
+@Slf4j
+public class CarConsumer {
+
+    @Value(value = "${topic.name}")
+    private String topic;
+
+    @Value(value = "${spring.kafka.consumer.group-id}")
+    private String groupId;
+
+    @KafkaListener(topics = "${topic.name}", groupId = "${spring.kafka.consumer.group-d", containerFactory = "carKafkaListenerContainerFactory")
+    public void ListenTopicCar(ConsumerRecord<String, CarDTO> record) {
+        log.info("Received Message" + record.partition());
+        log.info("Received Message" + record.value());
+
+    }
+
+}
